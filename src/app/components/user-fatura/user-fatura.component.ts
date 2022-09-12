ccb99a2f864435d329041f7894d68a9faede5a7b
armagann12 <armagansisik@gmail.com>
2022-09-12 11:56:12 +0300

    Implemented tables for invoices and made a filter button for the status of the invoice

--- src/app/components/user-fatura/user-fatura.component.ts
+++ src/app/components/user-fatura/user-fatura.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InvoiceService } from 'src/app/services/invoice.service';
 
 @Component({
@@ -7,13 +8,35 @@ import { InvoiceService } from 'src/app/services/invoice.service';
   styleUrls: ['./user-fatura.component.css']
 })
 export class UserFaturaComponent implements OnInit {
+  initData: any;
+  currentData:any;
+  selected: any = 'all';
+  displayedColumns: string[] = ['name', 'price', 'status'];
 
-  constructor(private invoiceService: InvoiceService) { }
+  constructor(private invoiceService: InvoiceService, private router: Router) { }
 
   ngOnInit(): void {
     this.invoiceService.getAllUsersInvoices().subscribe((res) =>{
       console.log(res)
+      this.initData = res
+      this.currentData = this.initData
     })
+  }
+
+  allData(){
+    this.currentData = this.initData
+  }
+
+  filterData(stat: any){
+    const newData = this.initData.filter((data :any) => data.status === stat)
+    this.currentData = newData
+  }
+  
+
+  logout() {
+    console.log("You are Logging Out")
+    localStorage.removeItem("token")
+    this.router.navigate(['login']);
   }
 
 }
