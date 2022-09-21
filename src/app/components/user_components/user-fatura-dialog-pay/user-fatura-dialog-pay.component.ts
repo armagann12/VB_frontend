1f66eacd7bc5bc25f757a0b09eeaad27cb7f440c
armagann12 <armagansisik@gmail.com>
2022-09-21 16:20:59 +0300

    Implemented all the functionality of creditCard except one

--- src/app/components/user_components/user-fatura-dialog-pay/user-fatura-dialog-pay.component.ts
+++ src/app/components/user_components/user-fatura-dialog-pay/user-fatura-dialog-pay.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { InvoiceService } from 'src/app/services/invoice.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-fatura-dialog-pay',
@@ -8,16 +11,41 @@ import { InvoiceService } from 'src/app/services/invoice.service';
   styleUrls: ['./user-fatura-dialog-pay.component.css']
 })
 export class UserFaturaDialogPayComponent implements OnInit {
+  invoicePrice: any
+  myCards: any = [];
+  chosenCard: any = undefined;
+  chosenCardDetail: any;
 
   constructor(public dialogRef: MatDialogRef<UserFaturaDialogPayComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private invoiceService: InvoiceService) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private invoiceService: InvoiceService, 
+    private userService: UserService, private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
-  }
-  
-  payInvoice(id: any){
-    this.invoiceService.payInvoice(id).subscribe((res) =>{
+    this.invoicePrice = this.data.price
+    this.userService.getMyCards().subscribe((res: any) => {
+      this.myCards = res
     })
+  }
+
+  payInvoice(id: any) {
+    const cardBalance = this.chosenCardDetail.balance
+
+    console.log(cardBalance, this.invoicePrice)
+
+    if (cardBalance < this.invoicePrice) {
+      this.toastr.error("Yetersiz Bakiye", "", { timeOut: 3000 })
+      this.router.navigate(['user/card']);
+      this.data.id = undefined
+    } else {
+      this.invoiceService.payInvoice(id).subscribe((res) =>{
+        //cardBalance azalıcak burada
+      })
+    }
+  }
+
+  chooseCard(id: any) {
+    this.chosenCard = id
+    this.chosenCardDetail = this.myCards.filter((el: any) => el.id === id)[0]
   }
 
 }
